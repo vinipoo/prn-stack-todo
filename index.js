063be3c8ef93026c0426d2f6d76aa49d90793574
unknown <yossi.povalaev@gmail.com>
2020-12-02 04:24:06 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+// process.env.NODE_ENV => production or undefined
+
+// middleware
+app.use(cors());
+app.use(express.json()); // req.body
+
+if (process.env.NODE_ENV === "production") {
+  // service static content
+  // npm run build
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+// ROUTES
+
+// get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * from todo");
+    res.status(200).json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.status(200).json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.status(200).json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.status(200).json("todo was updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.status(200).json("todo was deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is starting on port ${PORT}`);
+});
